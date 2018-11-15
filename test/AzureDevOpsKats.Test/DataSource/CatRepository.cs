9f72e15ce83f67193f53b166a451dc12ef278933
Stuart Shay <sshay@yahoo.com>
2018-11-15 00:26:50 -0500

    Add EF/InMemory

---
+++ test/AzureDevOpsKats.Test/DataSource/CatRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using AzureDevOpsKats.Data.Entities;
+using AzureDevOpsKats.Data.Repository;
+
+namespace AzureDevOpsKats.Test.DataSource
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CatRepository : ICatRepository
+    {
+        private readonly AzureDevOpsKatsContext _context;
+
+        public CatRepository(AzureDevOpsKatsContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cat> GetCats()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cat GetCat(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditCat(Cat cat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateCat(Cat cat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCat(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
