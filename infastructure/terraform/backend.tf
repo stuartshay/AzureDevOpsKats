e4d57ec8f312d03da63f4ea0b60c8778ad283e97
dpk <d.p.karpov@gmail.com>
2018-11-29 11:55:29 +0300

    update

--- infastructure/terraform/backend.tf
+++ infastructure/terraform/backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "terraformstatestor"
     container_name       = "azure-devops"
     key                  = "AzureDevOpsKats/AzureContainerServices/pipelines.terraform.tfstate"
+    access_key           = "0FUUmrDfreZ8sm/tQooZKJ2fYXpCEh6hMjL18BrpKdnDdy1D229RS94KZ1m7ja3CfG2V8CWVYq6nEQds9Kq2lg=="
   }
 }
