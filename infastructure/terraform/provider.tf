381081fdee93293dbf1f2948aab473668ca364d8
dpk <d.p.karpov@gmail.com>
2018-12-05 19:48:50 +0300

    rid username/password from kubernetes provider

--- infastructure/terraform/provider.tf
+++ infastructure/terraform/provider.tf
@@ -7,8 +7,8 @@ provider "kubernetes" {
 
   host    = "${azurerm_kubernetes_cluster.mod.kube_config.0.host}"
 
-  username               = "${azurerm_kubernetes_cluster.mod.kube_config.0.username}"
-  password               = "${azurerm_kubernetes_cluster.mod.kube_config.0.password}"
+  #username               = "${azurerm_kubernetes_cluster.mod.kube_config.0.username}"
+  #password               = "${azurerm_kubernetes_cluster.mod.kube_config.0.password}"
   client_certificate     = "${base64decode(azurerm_kubernetes_cluster.mod.kube_config.0.client_certificate)}"
   client_key             = "${base64decode(azurerm_kubernetes_cluster.mod.kube_config.0.client_key)}"
   cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.mod.kube_config.0.cluster_ca_certificate)}"
