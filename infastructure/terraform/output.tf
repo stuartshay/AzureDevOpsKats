583facf7c1c7531f26eb945ff5cb869067362692
dpk <d.p.karpov@gmail.com>
2018-12-06 16:48:43 +0300

    dns registartion, certificates, test deplyment

--- infastructure/terraform/output.tf
+++ infastructure/terraform/output.tf
@@ -2,6 +2,10 @@ output "kube_config" {
   value = "${azurerm_kubernetes_cluster.mod.kube_config_raw}"
 }
 
-output "host" {
+output "admin_host" {
   value = "${azurerm_kubernetes_cluster.mod.kube_config.0.host}"
+}
+
+output "ingress_host" {
+  value = "${dnsimple_record.test.hostname}"
 }
