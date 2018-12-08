cfb111d41483a8399dcd3edf5c1be8ff2941da8d
dpk <d.p.karpov@gmail.com>
2018-12-08 12:49:12 +0300

    update

--- infastructure/terraform/output.tf
+++ infastructure/terraform/output.tf
@@ -6,9 +6,9 @@ output "admin_host" {
   value = "${azurerm_kubernetes_cluster.mod.kube_config.0.host}"
 }
 
-#output "ingress_host" {
-#  value = "${dnsimple_record.test.hostname}"
-#}
+output "ingress_host" {
+  value = "${var.subdomain}.${var.domain_name}"
+}
 
 #output "ingress_ip_address" {
 #  value = "${azurerm_public_ip.nginx_ingress.ip_address}"
