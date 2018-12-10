f74e68a0ad2ea24a377bddc62cc8fbffa8ded1a5
dpk <d.p.karpov@gmail.com>
2018-12-10 16:03:36 +0300

    update

--- infastructure/terraform/k8s_namespace.tf
+++
@@ -1,5 +0,0 @@
-resource "kubernetes_namespace" "mod" {
-  metadata {
-    name = "${var.subdomain}"
-  }
-}
--- infastructure/terraform/provider.tf
+++ infastructure/terraform/provider.tf
@@ -2,18 +2,18 @@ provider "azurerm" {
   version = "1.19"
 }
 
-provider "kubernetes" {
-  version = "1.4.0"
+#provider "kubernetes" {
+#  version = "1.4.0"
 
-  host = "${azurerm_kubernetes_cluster.mod.kube_config.0.host}"
+#  host = "${azurerm_kubernetes_cluster.mod.kube_config.0.host}"
 
   #username               = "${azurerm_kubernetes_cluster.mod.kube_config.0.username}"
   #password               = "${azurerm_kubernetes_cluster.mod.kube_config.0.password}"
-  client_certificate = "${base64decode(azurerm_kubernetes_cluster.mod.kube_config.0.client_certificate)}"
+#  client_certificate = "${base64decode(azurerm_kubernetes_cluster.mod.kube_config.0.client_certificate)}"
 
-  client_key             = "${base64decode(azurerm_kubernetes_cluster.mod.kube_config.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.mod.kube_config.0.cluster_ca_certificate)}"
-}
+#  client_key             = "${base64decode(azurerm_kubernetes_cluster.mod.kube_config.0.client_key)}"
+#  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.mod.kube_config.0.cluster_ca_certificate)}"
+#}
 
 #provider "dnsimple" {
 #  version = "0.1.0"#
