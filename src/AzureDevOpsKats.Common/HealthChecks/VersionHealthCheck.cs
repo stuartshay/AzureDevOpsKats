127c539f65703ef89cfeacc6d79a1da1e7fe8e3b
Stuart Shay <sshay@yahoo.com>
2021-03-14 22:41:28 -0400

    Update

--- src/AzureDevOpsKats.Common/HealthChecks/VersionHealthCheck.cs
+++ src/AzureDevOpsKats.Common/HealthChecks/VersionHealthCheck.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,6 +12,16 @@ namespace AzureDevOpsKats.Common.HealthChecks
     /// </summary>
     public class VersionHealthCheck : IHealthCheck
     {
+        private readonly string _applicationVersionNumber;
+
+        private readonly DateTime _applicationBuildDate;
+
+        public VersionHealthCheck()
+        {
+            _applicationVersionNumber = Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
+            _applicationBuildDate = GetAssemblyLastModifiedDate();
+        }
+
         /// <summary>
         /// Application Version Health Check
         /// </summary>
@@ -18,10 +30,25 @@ namespace AzureDevOpsKats.Common.HealthChecks
         /// <returns></returns>
         public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
         {
-            var applicationVersionNumber = Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
+            var data = new Dictionary<string, object>
+            {
+                {"BuildDate", _applicationBuildDate},
+                {"BuildVersion", _applicationVersionNumber},
+            };
 
-            var result = Task.FromResult(string.IsNullOrEmpty(applicationVersionNumber) ? HealthCheckResult.Unhealthy("failed") : HealthCheckResult.Healthy(applicationVersionNumber));
-            return result;
+            var healthStatus = !string.IsNullOrEmpty(_applicationVersionNumber) ? HealthStatus.Healthy : HealthStatus.Degraded;
+            var healthCheckResult = new HealthCheckResult(healthStatus, _applicationVersionNumber, null, data); 
+
+            return Task.FromResult(healthCheckResult);
+        }
+
+        private DateTime GetAssemblyLastModifiedDate()
+        {
+            Assembly assembly = Assembly.GetEntryAssembly();
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(assembly?.Location ?? string.Empty);
+            DateTime lastModified = fileInfo.LastWriteTime;
+
+            return lastModified.ToUniversalTime();
         }
     }
 }
