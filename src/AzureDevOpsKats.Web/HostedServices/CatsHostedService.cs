ac9bdeb0c03728ff75ecd77ae71b35c1df1a3b9e
Stuart Shay <sshay@yahoo.com>
2021-12-30 15:38:42 -0500

    Update

--- src/AzureDevOpsKats.Web/HostedServices/CatsHostedService.cs
+++ src/AzureDevOpsKats.Web/HostedServices/CatsHostedService.cs
@@ -1,5 +1,4 @@
-﻿using Elastic.Apm;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using System;
 using System.Net.Http;
 using System.Threading;
@@ -30,28 +29,12 @@ namespace AzureDevOpsKats.Web.HostedServices
         /// <returns></returns>
         public async Task DoWork(CancellationToken cancellationToken)
         {
-            //var response = await Elastic.Apm.Agent.Tracer.CaptureTransaction("Console .Net Core Example", "background", async () =>
-            //{
-            //    Console.WriteLine("HostedService running");
-            //    // Make sure Agent.Tracer.CurrentTransaction is not null
-            //    var currentTransaction = Agent.Tracer.CurrentTransaction;
-            //    if (currentTransaction == null) throw new Exception("Agent.Tracer.CurrentTransaction returns null");
+            _logger.LogInformation("CatsHostedService|Time:{time}", DateTime.Now);
 
-            //    _logger.LogInformation("CatsHostedService|Time:{time}", DateTime.Now);
-
-
-            //    var httpClient = new HttpClient();
-            //    return await httpClient.GetAsync("https://github.com/stuartshay/AzureDevOpsKats", cancellationToken);
-            //});
+            var httpClient = new HttpClient();
+            await httpClient.GetAsync("https://github.com/stuartshay/AzureDevOpsKats", cancellationToken);
         }
     }
-
-
-
-
-
-
-
 
     /// <summary>
     /// 
