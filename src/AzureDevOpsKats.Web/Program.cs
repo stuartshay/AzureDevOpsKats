c671d3723ff007f575f0941d8bfe6a81a93fc887
Stuart Shay <sshay@yahoo.com>
2022-01-15 21:48:11 -0500

    Add Systems Mgr

--- src/AzureDevOpsKats.Web/Program.cs
+++ src/AzureDevOpsKats.Web/Program.cs
@@ -46,14 +46,14 @@ namespace AzureDevOpsKats.Web
              {
                  webBuilder.UseStartup<Startup>();
              })
-            
+
             // TODO: NOT CONFIGURED 
-            //.ConfigureAppConfiguration((context, builder) =>
-            // {
-            //   if (context.HostingEnvironment.EnvironmentName == "AwsEcs")
-            //   { builder.AddSystemsManager("/devopskats"); }
-            // }
-            //)
+            .ConfigureAppConfiguration((context, builder) =>
+             {
+                 if (context.HostingEnvironment.EnvironmentName == "AwsEcs")
+                 { builder.AddSystemsManager("/devopskats"); }
+             }
+            )
 
             .UseSerilog(Logging.ConfigureLogger);
     }
