8a807325f065e9af116d797a9628fc4869078f80
Stuart Shay <sshay@yahoo.com>
2022-01-17 14:31:11 -0500

    Test Parameter Store

---
+++ src/AzureDevOpsKats.Web/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using AzureDevOpsKats.Common.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace AzureDevOpsKats.Web.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/v{version:apiVersion}/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly string _server;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="settings"></param>
+        public ValuesController(IOptions<SmtpConfiguration> settings)
+        {
+            _server = settings.Value.Server;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public string Get()
+        {
+            var result = $"Server:{_server}";
+            return result;
+        }
+
+    }
+}
