55038b7cf960ed17acd796606984e796991e95b4
chiplonton <chipkogi010790@gmail.com>
2022-02-12 16:48:47 +0100

    infra structure - ec2

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -245,7 +245,7 @@ resource "aws_security_group" "efs" {
     security_groups = [
       aws_security_group.master_ecs_tasks.id,
       aws_security_group.ecs_tasks.id,
-      aws_security_group.jumpbox.id
+      #aws_security_group.jumpbox.id
     ]
   }
 
@@ -259,70 +259,70 @@ resource "aws_security_group" "efs" {
 }
 
 ## JumpBox
-resource "aws_security_group" "jumpbox" {
-  name   = "${local.name}-jumpbox"
-  vpc_id = data.aws_vpc.default.id
+# resource "aws_security_group" "jumpbox" {
+#   name   = "${local.name}-jumpbox"
+#   vpc_id = data.aws_vpc.default.id
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     protocol    = "tcp"
+#     from_port   = 22
+#     to_port     = 22
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    protocol    = "tcp"
-    from_port   = 5901
-    to_port     = 5901
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     protocol    = "tcp"
+#     from_port   = 5901
+#     to_port     = 5901
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    protocol         = "-1"
-    from_port        = 0
-    to_port          = 0
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
+#   egress {
+#     protocol         = "-1"
+#     from_port        = 0
+#     to_port          = 0
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+# }
 
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+# module "ec2_instance" {
+#   source  = "terraform-aws-modules/ec2-instance/aws"
+#   version = "~> 3.0"
 
-  name = "${local.name}-jumpbox"
+#   name = "${local.name}-jumpbox"
 
-  ami                    = "ami-04505e74c0741db8d"
-  instance_type          = "t2.small"
-  key_name               = "culiops"
-  monitoring             = false
-  vpc_security_group_ids = [aws_security_group.jumpbox.id]
-  subnet_id              = "subnet-f6c74afa"
+#   ami                    = "ami-04505e74c0741db8d"
+#   instance_type          = "t2.small"
+#   key_name               = "culiops"
+#   monitoring             = false
+#   vpc_security_group_ids = [aws_security_group.jumpbox.id]
+#   subnet_id              = "subnet-f6c74afa"
 
-  root_block_device = [
-    {
-      encrypted   = true
-      volume_type = "gp3"
-      throughput  = 200
-      volume_size = 30
-    },
-  ]
+#   root_block_device = [
+#     {
+#       encrypted   = true
+#       volume_type = "gp3"
+#       throughput  = 200
+#       volume_size = 30
+#     },
+#   ]
 
-  tags = {
-    Env = "develop"
-  }
+#   tags = {
+#     Env = "develop"
+#   }
 
-  volume_tags = {
-    Env = "develop"
-  }
-}
+#   volume_tags = {
+#     Env = "develop"
+#   }
+# }
 
-resource "aws_eip" "jumpbox" {
-  instance = module.ec2_instance.id
-  vpc      = true
+# resource "aws_eip" "jumpbox" {
+#   instance = module.ec2_instance.id
+#   vpc      = true
 
-  tags = {
-    Env = "develop"
-  }
-}
+#   tags = {
+#     Env = "develop"
+#   }
+# }
 
