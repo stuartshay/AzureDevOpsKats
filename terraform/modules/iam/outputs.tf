00af293aaabbac504d836939980ba39cda42ed26
Stuart Shay <sshay@yahoo.com>
2022-02-26 23:05:58 -0500

    Develop (#670)
    * Add Document
    
    * Update conda.md
    
    * Update Readme
    
    * Update Readme
    
    * Update Readme
    
    * Update Readme
    
    * Update Readme
    
    * Update Readme
    
    * Update Readme
    
    * Update Readme
    
    * Update Readme
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Rename to ECS Service Tasks
    
    * Update Documenation
    
    * Update Documenation
    
    * Update Documentation
    
    * Update Documentation
    
    * Update Documentation
    
    * Update Documentation
    
    * terraform code restructure
    
    * move back jumpbox
    
    * move jumpbox to backup place
    
    * Update Readme
    
    * Update
    
    * terraform restructure finished
    
    * workflow update to new strcture
    
    * workflow for ansible
    
    * update aws provider and profile
    
    * fixed terraform fmt
    
    * add aws cre
    
    * fixed workflow confition
    
    * Update Readme
    
    * Update Readme
    
    * ansible workflow update ansible lint
    
    * fixed terraform conflict file
    
    * Fix PR
    
    Co-authored-by: chiplonton <chipkogi010790@gmail.com>
    Co-authored-by: chiplonton <95140051+chiplonton@users.noreply.github.com>

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,10 +1,6 @@
 output "ecs_task_execution_role_arn" {
   description = "The ECS execution role arn"
-<<<<<<< HEAD
   value       = aws_iam_role.ecs_task_execution.arn
-=======
-  value       =  aws_iam_role.ecs_task_execution.arn
->>>>>>> origin/master
 }
 
 output "ecs_container_role_arn" {
