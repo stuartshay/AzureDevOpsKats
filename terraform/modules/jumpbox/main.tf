50f49f28ecb286ea776eeebddae1181ed1568043
chiplonton <chipkogi010790@gmail.com>
2022-03-02 14:55:49 +0100

    fixed ansible jumpbox

--- terraform/modules/jumpbox/main.tf
+++ terraform/modules/jumpbox/main.tf
@@ -32,6 +32,7 @@ module "ec2" {
   monitoring             = false
   vpc_security_group_ids = [module.security_group_jumpbox.id]
   subnet_id              = var.subnet_id
+  iam_instance_profile   = aws_iam_instance_profile.jumpbox.name
 
   root_block_device = [
     {
@@ -46,4 +47,33 @@ module "ec2" {
 resource "aws_eip" "this" {
   instance = module.ec2.id
   vpc      = true
+}
+
+resource "aws_iam_instance_profile" "jumpbox" {
+  name = var.name
+  role = aws_iam_role.jumpbox.name
+}
+
+resource "aws_iam_role" "jumpbox" {
+  name = "${var.name}-instance"
+
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ec2.amazonaws.com"
+     },
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "efs" {
+  role       = aws_iam_role.jumpbox.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonElasticFileSystemFullAccess"
 }
