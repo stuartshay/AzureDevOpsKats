b6c1b15c5072a7fcc2ab1c3a00c82550b73aa757
chiplonton <chipkogi010790@gmail.com>
2022-03-05 04:25:42 +0100

    fixed EFS missing subnets

--- terraform/modules/efs/main.tf
+++ terraform/modules/efs/main.tf
@@ -6,7 +6,9 @@ resource "aws_efs_file_system" "this" {
 }
 
 resource "aws_efs_mount_target" "this" {
+  count = length(var.subnet_ids)
+
   file_system_id  = aws_efs_file_system.this.id
-  subnet_id       = var.subnet_id
+  subnet_id       = var.subnet_ids[count.index]
   security_groups = var.security_group_ids
 }
